084f2ab139f7b3a11f108ad748ef18c96acc9c73
Alexander Polikarpov <alex-polikarpov@yandex.ru>
2023-11-20 23:19:00 +0700

    add context handle

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+from catalog.models import Product
 from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'catalog/index.html')
+    context = {
+        'object_list': Product.objects.all()
+    }
+    return render(request, 'catalog/index.html', context)
 
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
+
+def product(request, pk):
+    context = {
+        'object': Product.objects.get(pk=pk)
+    }
+    return render(request, 'catalog/product.html', context)
