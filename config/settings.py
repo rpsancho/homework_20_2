a5c81de2179166cfec3d2452402dd59e6822201a
Alexander Polikarpov <alex-polikarpov@yandex.ru>
2023-11-20 23:18:34 +0700

    nothing

--- config/settings.py
+++ config/settings.py
@@ -17,10 +17,6 @@ DB_PASSWORD = '1'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-)
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -130,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
